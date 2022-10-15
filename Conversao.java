9cc74c56ad4357fb21cffdfc31c2b00bb04e6185
VitorComin <106283734+VitorComin@users.noreply.github.com>
2022-10-14 21:22:08 -0300

    Update and rename Converter.java to Conversao.java

--- Conversao.java
+++ Conversao.java
@@ -2,29 +2,22 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-    public class Converter extends JFrame {
+    public class Conversao extends JFrame {
         private JComboBox input, output;
         private JTextField infoBlocs[];
-
-        public Converter() {
-            setTitle("[Conversor de temperatura]");
+        public Convert() {
+            setTitle("[Converter a temperatura]");
             setSize(250, 250);
-
-
-
             startFunc();
             setVisible(true);
         }
-
         private void startFunc() {
             setLayout(new FlowLayout(FlowLayout.CENTER));
-
             infoBlocs = new  JTextField[2];
             for(int i = 0; i < infoBlocs.length; i++){
                 infoBlocs[i] = new JTextField();
-                infoBlocs[i].setPreferredSize(new Dimension(100,50));
+                infoBlocs[i].setPreferredSize(new Dimension(50,50));
             }
-
             infoBlocs[1].setEditable(false);
             String temps[] = {"Celsius", "Fahrenheit", "Kelvin"};
             input = new JComboBox(temps);
@@ -33,7 +26,7 @@ import javax.swing.*;
             Container c = new Container();
             c.setLayout(new GridLayout(2, 2));
 
-            JButton conversor = new JButton("Ver temperatura");
+            JButton conversor = new JButton("Temperatura");
             conversor.addActionListener(new ActionListener() {
 
                 @Override
@@ -44,25 +37,18 @@ import javax.swing.*;
                         int c = output.getSelectedIndex();
                         if(b == c)
                             infoBlocs[1].setText(a + "");
-
-
                         else if(b == 0 && c == 1)
                             infoBlocs[1].setText((a * 1.8 + 32) + "");
                         else if(b == 0 && c == 2)
                             infoBlocs[1].setText((a + 273.15) + "");
-
-
                         else if(b == 1 && c == 0)
                             infoBlocs[1].setText(((a - 32) / 1.8) + "");
                         else if(b == 1 && c == 2)
                             infoBlocs[1].setText(((a - 32) / 1.8 + 273.15) + "");
-
-
                         else if(b == 2 && c == 0)
                             infoBlocs[1].setText((a - 273.15) + "");
                         else if(b == 2 && c == 1)
                             infoBlocs[1].setText((a - 273.15 - 32)/1.8 + "");
-
                     } catch (NumberFormatException e) {
                         throw new RuntimeException(e);
                     }
@@ -73,10 +59,10 @@ import javax.swing.*;
             c.add(input);
             c.add(new JLabel("Para"));
             c.add(output);
-
             add(infoBlocs[0]);
             add(c);
             add(conversor);
             add(infoBlocs[1]);
+            
         }
     }
